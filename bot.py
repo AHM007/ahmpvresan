335220809755adc16d9ed90055de25be15278365
AHM007 <amirhosseinmoaseri92@gmail.com>
2016-08-09 19:27:03 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,8 +13,8 @@ import logging
 import subprocess
 import requests
 import os
-token = "199356209:AAHMUXxTeFXnsqwWjZxJD2Zc" #token
-sudo = 122774063 #admin
+token = "147980566:AAEVkeTIeBvzkiarvaw17CAUhRj3cfCb3yA" #token
+sudo = 133839753 #admin
 bot = telebot.TeleBot(token)
 db = "https://api.telegram.org/bot{}/getMe?".format(token)
 res = urllib.request.urlopen(db)
